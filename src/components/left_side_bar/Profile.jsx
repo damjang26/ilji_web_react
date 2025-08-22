98f812d221e95ccb78ea18b3448bc7e6298d8a92
jaehoon <jesjang705@naver.com>
2025-08-22 17:25:29 +0900

    update ( modal-off add)

--- src/components/left_side_bar/Profile.jsx
+++ src/components/left_side_bar/Profile.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { FaSearch } from "react-icons/fa";
 import { CloseButton, Email, IconContainer, ImageWrapper, InfoWrapper, ModalHeader, Nickname, ProfileContainer, ProfileImageArea,
     SearchInput, SearchModal,} from "../../styled_components/left_side_bar/Profile.styles.jsx";
@@ -6,6 +6,21 @@ import { CloseButton, Email, IconContainer, ImageWrapper, InfoWrapper, ModalHead
 const Profile = () => {
 
     const [isModalSearch, setIsModalSearch] =useState(false);
+    const modalRef = useRef(null); // 모달 DOM을 참조 목적의 ref 생성
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {// 모달 열려 있고, 클릭된 곳이 모달 외부일 때
+            if (modalRef.current && !modalRef.current.contains(e.target)) {
+                setIsModalSearch(false);
+            }
+        };
+        if (isModalSearch) {// 모달 열릴 때 document에 이벤트 리스너 추가
+            document.addEventListener('mousedown', handleClickOutside);
+        }
+        return () => {// 컴포넌트가 언마운트, 모달이 닫힐 때 이벤트 리스너 제거
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isModalSearch]); // isModalSearch 상태가 변경될 때마다 이 훅을 실행
 
     return  (
         <ProfileContainer>
@@ -14,7 +29,7 @@ const Profile = () => {
                 <div>....</div>
             </IconContainer>
             {isModalSearch && (
-                <SearchModal>
+                <SearchModal ref={modalRef}>
                     <ModalHeader>
                         <span>검색</span>
                         <CloseButton onClick={()=>setIsModalSearch(false)}>닫기</CloseButton>
