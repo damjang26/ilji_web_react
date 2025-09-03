3c012d6647254f3632c5a0d8d0843edd1d950b4a
Stalae <qldmlskfl135@gmail.com>
2025-09-03 13:05:38 +0900

    9/3 1:5 모달 밖에서 마우스 떼면 모달창 꺼지는 이슈 해결 , 마이페이지, 왼쪽 사이드 바 디자인 일부 수정

--- src/components/left_side_bar/TabMenu.jsx
+++ src/components/left_side_bar/TabMenu.jsx
@@ -1,25 +1,18 @@
 import {Link} from "react-router-dom";
-import styled from "styled-components";
 import React from "react";
-import {ToggleButton} from "../../styled_components/left_side_bar/TabMenuStyled.jsx";
-import {FaBell, FaCalendarAlt} from "react-icons/fa";
+import {
+    MenuLink,
+    TabMenuContainer,
+    ToggleButton
+} from "../../styled_components/left_side_bar/TabMenuStyled.jsx";
+import {FaBell, FaCalendarAlt, FaListAlt} from "react-icons/fa";
 
-const TabMenuContainer = styled.div`
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center; /* 자식 요소들을 수직 중앙에 배치 */
-    align-items: center; /* 자식 요소들을 수평 중앙에 배치 */
-    gap: 15px; /* 요소들 사이의 간격 */
-`;
 
 const TabMenu = ({toggleButtonRef, onToggle}) => {
     return (
         <TabMenuContainer>
-            <hr/>
-            <div>Tab Menu</div>
-            <Link to="/"><FaCalendarAlt/> Calendar</Link>
-            <Link to="/post">Post</Link>
+            <MenuLink to="/"><FaCalendarAlt/> Calendar</MenuLink>
+            <MenuLink to="/post"><FaListAlt/> Post</MenuLink>
             <ToggleButton ref={toggleButtonRef} onClick={onToggle}>
                 <FaBell/> Notifications
             </ToggleButton>
