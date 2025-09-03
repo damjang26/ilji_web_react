f728e8bea751d632aedbf7cc9a367962169ee55d
Stalae <qldmlskfl135@gmail.com>
2025-09-03 13:18:38 +0900

    111111

--- src/components/left_side_bar/TabMenu.jsx
+++ src/components/left_side_bar/TabMenu.jsx
@@ -16,6 +16,7 @@ const TabMenu = ({toggleButtonRef, onToggle}) => {
             <ToggleButton ref={toggleButtonRef} onClick={onToggle}>
                 <FaBell/> Notifications
             </ToggleButton>
+            
         </TabMenuContainer>
     );
 };
