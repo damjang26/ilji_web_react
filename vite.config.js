80a23201964e2b6b03a63f73236df3c56b1148c5
cbj <damjang26@gmail.com>
2025-09-30 15:47:16 +0900

    deploy8

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/ilji_web_react/',
     define: {
         global: 'window',
     },
